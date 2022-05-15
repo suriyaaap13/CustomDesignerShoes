afe81624d98aee61943142d1e7e91fecdeaa5ead
suriyaaap13 <suriyaaap13@gmail.com>
2022-05-15 15:14:00 +0530

    Set underline for navigation bar

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ export default function Home() {
     <div class="p-5 m-5">
       <h1 class="p-5 m-5">
         This is Home.
+        Go to the Store and Design your own pair of Sneakers
       </h1>
     </div>
   )

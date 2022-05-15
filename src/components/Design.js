2371f2a40ed460653f7e224457f2ef378b7d5a2b
suriyaaap13 <suriyaaap13@gmail.com>
2022-05-15 08:30:27 +0530

    Add select color for the shoe

--- src/components/Design.js
+++ src/components/Design.js
@@ -1,14 +1,32 @@
-import React from 'react'
-// <i class="fa-solid fa-angle-left"></i>
+import React, {useState} from 'react'
+import Color from './Color';
+
 export default function Design(props) {
+  const [FB, setFB] = useState(true);
+  const [back, setBack] = useState(true);
+  const [sole, setSole] = useState(true);
+  const [size, setSize] = useState(true);
+
+  function handleClickFB(){
+    setFB(!FB);
+  }
+
+  function handleClickBack(){
+    setBack(!back);
+  }
+
+  function handleClickSole() {
+    setSole(!sole);
+  }
+
   return (
     <>
     <div className='col-md-10'>
       <div className='shadow p-4 mb-3 bg-light rounded'>
-          {/* shop and Shop Icon */}
+          {/* Back Icon and Page name */}
           <div className='d-flex justify-content-start align-items-enter'>
             <div className='fs-4 mx-3' onClick={props.tab} style={{cursor: "pointer"}}>
-              <i class="fa-solid fa-angle-left"></i>
+              <i className="fa-solid fa-angle-left"></i>
             </div>
             <div className='fs-4'>
               Your Design Space
@@ -16,11 +34,11 @@ export default function Design(props) {
           </div>
           
           <div className='py-4 px-4 row'>
-            <div className='col-8'> 
+            <div className='col-md-8'> 
               {/* Product Image */}
-              <img src={require('../shoes/shoe1.png')} alt='Product Image'/>
+              <img src={require('../shoes/shoe1.png')} style={{width: "40rem", height: "40rem"}} alt='Product Image'/>
             </div>
-            <div className='col-4 p-2'>
+            <div className='col-md-4 p-2'>
               {/* Product Deatails */}
               <div className='py-2'>
                 <div className='fs-3'>
@@ -40,44 +58,36 @@ export default function Design(props) {
                 </small>
               </div>
               {/* Product Design Choosing section */}
-              <div>
+              <div className='py-4'>
+              {/* Front and Back */}
                 <div className='row py-2'>
                   <div className='col-5 fs-5'>
                     Front & Back
                   </div>
-                  <div className='col-3 d-flex justify-content-between'>
-                    <div className='border border-dark' style={{width: "1.5rem", height: "1.5rem", backgroundColor: "white"}}></div>
-                    <div className='' style={{width: "1.5rem", height: "1.5rem", backgroundColor: "red"}}></div>
-                  </div>
+          
+                  <Color color={FB} changeColor={handleClickFB}/>
+                  
                 </div>
+                {/* Middle */}
                 <div className='row py-2'>
                   <div className='col-5 fs-5'>
                     Middle
                   </div>
-                  <div className='col-3 d-flex justify-content-between'>
-                    <div className='border border-dark' style={{width: "1.5rem", height: "1.5rem", backgroundColor: "white"}}></div>
-                    <div className='' style={{width: "1.5rem", height: "1.5rem", backgroundColor: "red"}}></div>
-                  </div>
+                  
+                  <Color color={back} changeColor = {handleClickBack} />
+                    
                 </div>
+                {/* Sole */}
                 <div className='row py-2'>
                   <div className='col-5 fs-5'>
                     Sole
                   </div>
-                  <div className='col-3 d-flex justify-content-between'>
-                    <div className='border border-dark' style={{width: "1.5rem", height: "1.5rem", backgroundColor: "white"}}></div>
-                    <div className='' style={{width: "1.5rem", height: "1.5rem", backgroundColor: "red"}}></div>
-                  </div>
+                  
+                  <Color color={sole} changeColor = {handleClickSole} />
+                  
                 </div>
-                <div className='row py-2'>
-                  <div className='col-5 fs-5'>
-                    Front & Back
-                  </div>
-                  <div className='col-3 d-flex justify-content-between'>
-                    <div className='border border-dark' style={{width: "1.5rem", height: "1.5rem", backgroundColor: "white"}}></div>
-                    <div className='' style={{width: "1.5rem", height: "1.5rem", backgroundColor: "red"}}></div>
-                  </div>
-                </div>
-                <div className=' py-2'>
+                {/* Size */}
+                <div className='py-2'>
                   <div className='col-5 fs-5 py-2'>
                     Size
                   </div>
@@ -92,12 +102,6 @@ export default function Design(props) {
 
             </div>
           </div>
-          
-          {/* Price */}
-          {/* Offers by Bank */}
-          {/* Front and Back */}
-          {/* Middle */}
-          {/* Size */}
       </div>
       {/* Add to cart Button */}
     </div>
